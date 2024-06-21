ff5467081ff7dc00b46ed5fdc3db817b03c947eb
Pallavi <pallavishettigar03@gmail.com>
2024-06-21 18:07:36 +0530

    add commit

---
+++ max min.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array = np.array([10, 20, 30, 40, 50])
+max_element = np.max(array)
+min_element = np.min(array)
+
+print("Max element:", max_element)
+print("Min element:", min_element)
